4eba6bef1dd9eb56da23fd248be1e48cf78fef9d
Leon Bowie <leon@Leons-MacBook-Pro.local>
2020-06-18 13:56:57 +1200

    remove unneeded terms

--- obsidion/utils/utils.py
+++ obsidion/utils/utils.py
@@ -1,6 +1,3 @@
-from typing import Optional, SupportsInt
-
-
 async def get(session, url: str, params=None, json=None):
     """get the json from a webpage"""
     async with session.get(url, params=params, json=json) as resp:
