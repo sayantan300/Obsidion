a03e40b9b72fb0d28330f52156a831c9df1bbc5a
Darkflame72 <leon@MacBook-Pro.localdomain>
2020-07-03 23:13:46 +1200

    little bit of code cleanup

--- obsidion/core/minecraft_news.py
+++ obsidion/core/minecraft_news.py
@@ -1,16 +1,16 @@
 import logging
 from datetime import datetime
-import discord
 from time import mktime
 
+import bs4
+import discord
 import feedparser
 from discord.ext import commands, tasks
 
 from obsidion.bot import Obsidion
 
-categories = ("Deep Dives", "News", "Minecraft Builds", "Dungeons")
-
 Minecraft_News_RSS = "https://www.minecraft.net/en-us/feeds/community-content/rss"
+Categories = ("Minecraft Builds", "News", "Deep Dives", "Guides")
 
 log = logging.getLogger(__name__)
 
@@ -20,6 +20,7 @@ class MinecraftNews(commands.Cog):
 
     def __init__(self, bot: Obsidion):
         self.bot = bot
+        self.last_data = datetime.now()
         self.get_media.start()
 
     @tasks.loop(minutes=10)
@@ -27,26 +28,48 @@ class MinecraftNews(commands.Cog):
         async with self.bot.http_session.get(Minecraft_News_RSS) as resp:
             text = await resp.text()
         data = feedparser.parse(text)
+
+        # select the most recent post
         latest_post = data["entries"][0]
+
+        # run checks to see wether it should be posted
+
+        time = datetime.fromtimestamp(mktime(latest_post["published_parsed"]))
+
+        if time < self.last_data:
+            return
+        # create discord embed
         description = f"Summary: {latest_post['summary']}"
+
         embed = discord.Embed(
-            title=latest_post["title_detail"]["value"],
+            title=latest_post["title_detail"]["value"]
+            .replace("--", ": ")
+            .replace("-", " ")
+            .upper(),
             description=description,
             colour=0x00FF00,
             url=latest_post["id"],
         )
+
+        # add categories
         embed.set_image(url=f"https://minecraft.net{latest_post['imageurl']}")
         embed.add_field(name="Category", value=latest_post["primarytag"])
+        # author info
+        # embed.set_thumbnail =
+        # embed.add_field(name="Author:", value)
+
+        # create footer
         embed.set_footer(text="Article Published")
+        embed.timestamp = time
+
+        # create title
         embed.set_author(
             name="New Article on Minecraft.net",
             url=f"https://minecraft.net{latest_post['imageurl']}",
             icon_url="https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/img/menu/menu-buy--reversed.gif",
         )
-        dt = datetime.fromtimestamp(mktime(latest_post["published_parsed"]))
 
-        embed.timestamp = dt
-
+        # send embed
         channel = self.bot.get_channel(725790318938685520)
         await channel.send(embed=embed)
 
