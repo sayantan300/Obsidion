8814f59df8adeae9a71706aeab3ee8cb1d848d9d
Darkflame72 <leon@neptune.localdomain>
2020-07-09 17:54:58 +1200

    create nicer embeds

--- obsidion/cogs/servers/servers.py
+++ obsidion/cogs/servers/servers.py
@@ -20,44 +20,6 @@ class servers(commands.Cog):
             return get_uuid(ctx.bot.http_session, username)
 
     @commands.command()
-    async def minesaga(self, ctx: commands.Context, username: str):
-        """get stats from minesage"""
-        await ctx.trigger_typing()
-        data = await minesaga(username, ctx.bot.http_session)
-        if not data:
-            await ctx.send(
-                f"`{username}` has not logged onto Minesaga or there are no stats"
-            )
-            return
-        embed = discord.Embed(title=f"`{username}`'s Minesaga Stats", color=0x00FF00)
-        embed.add_field(
-            name="Online Stats",
-            value=(
-                f"Joined: `{data['joined']}`\nLast Seen: `{data['last_seen']}`\nPlay Time: `{data['play_time']}`"
-            ),
-        )
-        embed.add_field(
-            name="Island Stats",
-            value=(
-                f"Island: `{data['game_stats'][0]['Island']['Island']}`\nIsland Rank: `{data['game_stats'][0]['Island']['Island rank']}`\nIsland Position: `{data['game_stats'][0]['Island']['Island position']}`"
-            ),
-        )
-        embed.add_field(
-            name="General Stats",
-            value=(
-                f"Playtime: `{data['game_stats'][1]['General']['Playtime']}`\nBalance: `{data['game_stats'][1]['General']['Balance']}`\nCoinflips Won: `{data['game_stats'][1]['General']['Coinflips Won']}`\nJackpots Won: `{data['game_stats'][1]['General']['Jackpots Won']}`"
-            ),
-        )
-        embed.add_field(
-            name="Combat Stats",
-            value=(
-                f"Kills: `{data['game_stats'][2]['Combat']['Kills']}`\nDeaths: `{data['game_stats'][2]['Combat']['Deaths']}`\nKDR: `{data['game_stats'][2]['Combat']['KDR']}`\nBosses Killed: `{data['game_stats'][2]['Combat']['Bosses Killed']}`\nTotla Mob Kills: `{data['game_stats'][2]['Combat']['Total Mob Kills']}`"
-            ),
-        )
-
-        await ctx.send(embed=embed)
-
-    @commands.command()
     async def wyncraft(self, ctx: commands.Context, username: str):
         await ctx.trigger_typing()
         data = await wyncraftClasses(username, ctx.bot.http_session)
@@ -66,16 +28,28 @@ class servers(commands.Cog):
                 f"`{username}` has not logged onto Wynncraft or their status is not available."
             )
             return
-        len_data = len(data['classes'])
-        embed = discord.Embed(
-            title=f"`{username}`'s Wynncraft Classes", color=0xA4EC66
+        len_data = len(data["classes"])
+        embed = discord.Embed(color=0xA4EC66)
+        embed.set_author(
+            name=f"WynnCraft information for {username}",
+            url=f"https://wynncraft.com/stats/player/{username}",
+            icon_url="https://cdn.wynncraft.com/img/wynn.png",
         )
+        embed.set_thumbnail(
+            url=f"https://visage.surgeplay.com/bust/{await get_uuid(ctx.bot.http_session, username)}"
+        )
+        embed.timestamp = ctx.message.created_at
         for i in range(len_data):
             embed.add_field(
-                name=data['classes'][i]['class_name'],
-                value=(f"Class Name: `{data['classes'][i]['class_name']}`\nClass Level: `{data['classes'][i]['class_level']}`\nClass Deaths: `{data['classes'][i]['class_deaths']}`"),
+                name=data["classes"][i]["class_name"],
+                value=(
+                    f"Class Name: `{data['classes'][i]['class_name']}`\n"
+                    f"Class Level: `{data['classes'][i]['class_level']}`\n"
+                    f"Class Deaths: `{data['classes'][i]['class_deaths']}`"
+                ),
             )
         await ctx.send(embed=embed)
+
     @commands.command()
     async def gommehd(self, ctx: commands.Context, username: str):
         await ctx.trigger_typing()
@@ -85,11 +59,18 @@ class servers(commands.Cog):
                 f"`{username}` has not logged onto GommeHD or their status is not available."
             )
             return
-        embed = discord.Embed(
-            title=f"`{username}`'s GommeHD Games", color=0xF1A90F
+        embed = discord.Embed(color=0xF1A90F)
+        embed.set_author(
+            name=f"GommeHD information for {username}",
+            url=f"https://www.gommehd.net/player/index?playerName={username}",
+            icon_url="https://www.gommehd.net/images/brandmark@3x.png",
         )
+        embed.set_thumbnail(
+            url=f"https://visage.surgeplay.com/bust/{await get_uuid(ctx.bot.http_session, username)}"
+        )
+        embed.timestamp = ctx.message.created_at
         for game in data["game_stats"]:
-            value=""
+            value = ""
             name = list(game)
             name_new = name[0]
             scores = game[name_new]
@@ -97,6 +78,7 @@ class servers(commands.Cog):
                 value += f"{key}: {scores[key]}\n"
             embed.add_field(name=name_new, value=value)
         await ctx.send(embed=embed)
+
     @commands.command()
     async def veltpvp(self, ctx: commands.Context, username: str):
         await ctx.trigger_typing()
@@ -106,18 +88,30 @@ class servers(commands.Cog):
                 f"`{username}` has not logged onto VeltPVP or their status is not available."
             )
             return
-        embed = discord.Embed(
-            title=f"`{username}`'s VeltPVP Stats", color=0x2E39A7
+        embed = discord.Embed(color=0x2E39A7)
+        embed.set_author(
+            name=f"VeltPVP information for {username}",
+            url=f"https://www.veltpvp.com/u/{username}",
+            icon_url="https://www.veltpvp.com/resources/images/nav-logo.png",
         )
+        embed.set_thumbnail(
+            url=f"https://visage.surgeplay.com/bust/{await get_uuid(ctx.bot.http_session, username)}"
+        )
+        embed.timestamp = ctx.message.created_at
         embed.add_field(
             name=("VeltPVP Stats"),
-            value=(f"Rank: `{data['rank']}`\nLast Seen: `{data['last_seen']}`\nCurrent Status: `{data['current_status']}`\nFirst Joined: `{data['first_joined']}`\nTime Played: `{data['time_played']}`"),
+            value=(
+                f"Rank: `{data['rank']}`\nLast Seen: `{data['last_seen']}`\nCurrent Status: `{data['current_status']}`\nFirst Joined: `{data['first_joined']}`\nTime Played: `{data['time_played']}`"
+            ),
         )
         embed.add_field(
             name=("VeltPVP HCF Stats"),
-            value=(f"Kills: `{data['game_stats'][0]['HCF']['Kills']}`\nDeaths: `{data['game_stats'][0]['HCF']['Deaths']}`\nKDR: `{data['game_stats'][0]['HCF']['KDR']}`"),
+            value=(
+                f"Kills: `{data['game_stats'][0]['HCF']['Kills']}`\nDeaths: `{data['game_stats'][0]['HCF']['Deaths']}`\nKDR: `{data['game_stats'][0]['HCF']['KDR']}`"
+            ),
         )
         await ctx.send(embed=embed)
+
     @commands.command()
     async def blocksmc(self, ctx: commands.Context, username: str):
         await ctx.trigger_typing()
@@ -127,11 +121,18 @@ class servers(commands.Cog):
                 f"`{username}` has not logged onto VeltPVP or their status is not available."
             )
             return
-        embed = discord.Embed(
-            title=f"`{username}`'s BlocksMC Stats", color=0xF1A90F
+        embed = discord.Embed(color=0xF1A90F)
+        embed.set_author(
+            name=f"BlocksMC information for {username}",
+            url=f"https://blocksmc.com/player/{username}",
+            icon_url="https://blocksmc.com/templates3/src/logo-gray-sm.png",
         )
+        embed.set_thumbnail(
+            url=f"https://visage.surgeplay.com/bust/{await get_uuid(ctx.bot.http_session, username)}"
+        )
+        embed.timestamp = ctx.message.created_at
         for game in data["game_stats"]:
-            value=""
+            value = ""
             name = list(game)
             name_new = name[0]
             scores = game[name_new]
@@ -139,6 +140,7 @@ class servers(commands.Cog):
                 value += f"{key}: {scores[key]}\n"
             embed.add_field(name=name_new, value=value)
         await ctx.send(embed=embed)
+
     @commands.command()
     async def universocraft(self, ctx: commands.Context, username: str):
         await ctx.trigger_typing()
@@ -148,11 +150,18 @@ class servers(commands.Cog):
                 f"`{username}` has not logged onto VeltPVP or their status is not available."
             )
             return
-        embed = discord.Embed(
-            title=f"`{username}`'s BlocksMC Stats", color=0xF1A90F
+        embed = discord.Embed(color=0xF1A90F)
+        embed.set_author(
+            name=f"UniversoCraft information for {username}",
+            url=f"https://www.universocraft.com/members/{username}",
+            icon_url="https://www.universocraft.com/favicon.ico",
         )
+        embed.set_thumbnail(
+            url=f"https://visage.surgeplay.com/bust/{await get_uuid(ctx.bot.http_session, username)}"
+        )
+        embed.timestamp = ctx.message.created_at
         for game in data["game_stats"]:
-            value=""
+            value = ""
             name = list(game)
             name_new = name[0]
             scores = game[name_new]
@@ -160,7 +169,7 @@ class servers(commands.Cog):
                 value += f"{key}: {scores[key]}\n"
             embed.add_field(name=name_new, value=value)
         await ctx.send(embed=embed)
-        
+
     @commands.command()
     async def minesaga(self, ctx: commands.Context, username: str):
         await ctx.trigger_typing()
@@ -170,11 +179,18 @@ class servers(commands.Cog):
                 f"`{username}` has not logged onto Minesaga or their status is not available."
             )
             return
-        embed = discord.Embed(
-            title=f"`{username}`'s Minesaga Stats", color=0xF1A90F
+        embed = discord.Embed(color=0xF1A90F)
+        embed.set_author(
+            name=f"Minesaga information for {username}",
+            url=f"https://www.minesaga.org/members/{username}",
+            icon_url="https://www.minesaga.org/favicon.ico",
         )
+        embed.set_thumbnail(
+            url=f"https://visage.surgeplay.com/bust/{await get_uuid(ctx.bot.http_session, username)}"
+        )
+        embed.timestamp = ctx.message.created_at
         for game in data["game_stats"]:
-            value=""
+            value = ""
             name = list(game)
             name_new = name[0]
             scores = game[name_new]
